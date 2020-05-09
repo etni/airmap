2767e72fc16551977afd7aa1d01042e4f6376c1b
Etni Alvis <etni.alvis@gmail.com>
2020-05-09 15:30:56 -0400

    Initial

---
+++ src/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Interview
+{
+    public static class Program
+    {
+
+        static AirMap controller = new AirMap();
+
+        static void Main(string[] args)
+        {
+
+            var routes = new (string,string)[]
+            {
+                ("a", "b"),
+                ("b", "a"),
+                ("a", "c"),
+                ("c", "a"),
+                ("a", "d"),
+                ("d", "a"),
+                ("b","c"),
+                ("c", "b"),
+                ("b","d"),
+                ("d","b")
+            };
+
+
+
+            foreach(var (from,to)  in routes)
+                controller.AddRoute(from,to);
+
+            ProcessCommands();
+
+            //controller.PrintDirectDestinations();
+
+            //controller.PrintRoutes("c","d");
+        }
+
+        public static void ProcessCommands()
+        {
+            var done = false;
+            do
+            {
+                Console.Write("\nAdd/Query/Print? [A,Q,P] (escape to exit) ");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Escape:
+                        done = true;
+                        break;
+
+                    case ConsoleKey.P:
+                        controller.PrintDirectDestinations();
+                        break;
+
+                    case ConsoleKey.A:
+                        AddRoute();
+                        break;
+
+                    case ConsoleKey.Q:
+                        Query();
+                        break;
+
+                }
+                Console.Write("\n");
+
+            } while (!done);
+        }
+
+        public static void AddRoute()
+        {
+            Console.Write("\nOrigin     : ");
+            var from = Console.ReadLine();
+
+            Console.Write("Destination: ");
+            var destination = Console.ReadLine();
+
+            controller.AddRoute(from, destination);
+        }
+
+        public static void Query()
+        {
+            Console.Write("\nOrigin     : ");
+            var from = Console.ReadLine();
+
+            Console.Write("Destination: ");
+            var destination = Console.ReadLine();
+
+            controller.PrintRoutes(from, destination);
+        }
+
+
+
+
+    }
+}
